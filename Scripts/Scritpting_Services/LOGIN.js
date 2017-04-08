bd40cee2b09404f4d101ec277fb497f83a011459
Adam Hajduk <adam.hajduk@gmail.com>
2017-04-08 12:17:17 +0200

    Small fixes to LOGIN script

--- Scripts/Scritpting_Services/LOGIN.js
+++ Scripts/Scritpting_Services/LOGIN.js
@@ -8,25 +8,30 @@ if (event.request.method !== "POST") {
 }
 
 var url = "user/session";
+var host = "http://graymanix.ovh/api/v2/";
+var apiKeyScript = "&api_key=486bcd2b0b7cc55fbc3c16f1aadf041686d9cf68ce726b55c7c4012bddaab0fe";
 var options = null;
 
 platform.api.post(url, event.request.payload, '', function (body, response) {
     if (response.statusCode == 200) {
         event.setResponse(JSON.parse(body), 200, 'application/json');
         //sprawdzenie aktywnej licencji
-        url = "http://graymanix.ovh/api/v2/SIDM/_table/USERS"
+        url = host
+            + "SIDM/_table/USERS"
             + "?filter=SYSTEM_ID%3D"
             + JSON.parse(body).id
-            + "&api_key=486bcd2b0b7cc55fbc3c16f1aadf041686d9cf68ce726b55c7c4012bddaab0fe";
+            + apiKeyScript;
         platform.api.get(url, '', function (body, response) {
             if (response.statusCode == 200
                 && JSON.parse(body).resource.length == 1
                 && JSON.parse(body).resource[0].SERIAL_CODE_ID != null) {
 
-                url = "http://graymanix.ovh/api/v2/SIDM/_table/SERIAL_CODES"
+                url = host
+                    + "SIDM/_table/SERIAL_CODES"
                     + "?filter=ID%3D"
                     + JSON.parse(body).resource[0].SERIAL_CODE_ID
-                    + ")&api_key=486bcd2b0b7cc55fbc3c16f1aadf041686d9cf68ce726b55c7c4012bddaab0fe";
+                    + ")"
+                    + apiKeyScript;
 
                 platform.api.get(url, '', function (body, response) {
                     if (response.statusCode == 200
@@ -43,25 +48,31 @@ platform.api.post(url, event.request.payload, '', function (body, response) {
                         event.response.content.LICENSE = false;
                         event.setResponse(event.response.content, 200, 'application/json');
                     } else {
-                        event.setResponse({"errorType": "Something_went_wrong"}, 500, 'application/json');
+                        event.setResponse({"errorType": "somethingWentWrogn"}, 500, 'application/json');
                     }
                 });
+            } else if (response.statusCode == 200
+                && JSON.parse(body).resource.length == 1
+                && JSON.parse(body).resource[0].SERIAL_CODE_ID == null) {
+                event.response.content.LICENSE = false;
+                event.setResponse(event.response.content, 200, 'application/json');
             } else {
-                event.setResponse({"errorType": "User_not_in_table"}, 500, 'application/json');
+                event.setResponse({"errorType": "userNotInTable"}, 404, 'application/json');
             }
         });
     } else {
         //sprawdzenie powodu niezalogowania
-        url = "http://graymanix.ovh/api/v2/system/user?fields=last_login_date%2Cis_active%2Cconfirm_code&filter=email%3D"
+        url = host
+            + "system/user?fields=last_login_date%2Cis_active%2Cconfirm_code&filter=email%3D"
             + event.request.payload.email
-            + "&api_key=486bcd2b0b7cc55fbc3c16f1aadf041686d9cf68ce726b55c7c4012bddaab0fe";
+            + apiKeyScript;
 
         platform.api.get(url, '', function (body, response) {
             if (response.statusCode == 200
                 && JSON.parse(body).resource.length == 1
                 && JSON.parse(body).resource[0].is_active == 0) {
 
-                event.setResponse({"errorType": "account_banned"}, 500, 'application/json');
+                event.setResponse({"errorType": "accountBanned"}, 403, 'application/json');
 
             } else if (response.statusCode == 200
                 && JSON.parse(body).resource.length == 1
@@ -69,15 +80,15 @@ platform.api.post(url, event.request.payload, '', function (body, response) {
                 && JSON.parse(body).resource[0].confirm_code != "y"
                 && JSON.parse(body).resource[0].last_login_date == null) {
 
-                event.setResponse({"errorType": "account_not_activated"}, 500, 'application/json');
+                event.setResponse({"errorType": "accountNotActivated"}, 401, 'application/json');
 
             } else if (response.statusCode == 200 && JSON.parse(body).resource.length == 0) {
 
-                event.setResponse({"errorType": "account_not_exist"}, 500, 'application/json');
+                event.setResponse({"errorType": "accountNotExist"}, 404, 'application/json');
 
             } else if (response.statusCode == 200) {
 
-                event.setResponse({"errorType": "wrong_password"}, 500, 'application/json');
+                event.setResponse({"errorType": "wrongPassword"}, 401, 'application/json');
 
             } else {
 
