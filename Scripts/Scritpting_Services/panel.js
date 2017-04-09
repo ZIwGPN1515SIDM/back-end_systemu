b0bfa2478f147b4a8ce03914b01452a87e5486e2
Adam Hajduk <adam.hajduk@gmail.com>
2017-04-09 18:11:54 +0200

    Add script for getting and adding places in admin panel

---
+++ Scripts/Scritpting_Services/panel.js
@@ -0,0 +1,188 @@
+var host = "http://graymanix.ovh/api/v2/";
+var apiKeyScript = "api_key=486bcd2b0b7cc55fbc3c16f1aadf041686d9cf68ce726b55c7c4012bddaab0fe";
+
+if (event.request.method == "GET" && event.resource == "place") {
+    var url = host +
+        "user/session?session_token=" +
+        event.request.headers["x-dreamfactory-session-token"][0];
+    platform.api.get(url, '', function (body, response) {
+        if (response.statusCode == 200) {
+            url = host +
+                "sidm/_table/USERS?filter=SYSTEM_ID%3D" +
+                JSON.parse(body).id +
+                "&" + apiKeyScript;
+            platform.api.get(url, '', function (body, response) {
+                if (response.statusCode == 200) {
+                    url = host +
+                        "sidm/_table/PLACES" +
+                        "?filter=OWNER_ID%3D" +
+                        JSON.parse(body).resource[0].ID +
+                        "&" + apiKeyScript;
+                    platform.api.get(url, '', function (body, response) {
+                        if (response.statusCode == 200) {
+                            event.setResponse(JSON.parse(body), response.statusCode, 'applicaton/json');
+                        } else {
+                            event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                        }
+                    });
+                } else {
+                    event.setResponse({
+                        "error": {
+                            "context": null,
+                            "message": "User does not exists",
+                            "code": 404
+                        }
+                    }, 404, 'application/json');
+                }
+            });
+        } else {
+            event.setResponse({
+                "error": {
+                    "context": null,
+                    "message": "Session token does not exists",
+                    "code": 404
+                }
+            }, 404, 'application/json');
+        }
+    });
+} else if (event.request.method == "GET" && event.resource == "namespace") {
+    var url = host +
+        "user/session?session_token=" +
+        event.request.headers["x-dreamfactory-session-token"][0];
+    platform.api.get(url, '', function (body, response) {
+        if (response.statusCode == 200) {
+            url = host +
+                "sidm/_table/USERS?filter=SYSTEM_ID%3D" +
+                JSON.parse(body).id +
+                "&" + apiKeyScript;
+            platform.api.get(url, '', function (body, response) {
+                if (response.statusCode == 200) {
+                    url = host +
+                        "sidm/_table/NAMESPACES" +
+                        "?filter=OWNER_ID%3D" +
+                        JSON.parse(body).resource[0].ID +
+                        "&" + apiKeyScript;
+                    platform.api.get(url, '', function (body, response) {
+                        if (response.statusCode == 200) {
+                            event.setResponse(JSON.parse(body), response.statusCode, 'applicaton/json');
+                        } else {
+                            event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                        }
+                    });
+                } else {
+                    event.setResponse({
+                        "error": {
+                            "context": null,
+                            "message": "User does not exists",
+                            "code": 404
+                        }
+                    }, 404, 'application/json');
+                }
+            });
+        } else {
+            event.setResponse({
+                "error": {
+                    "context": null,
+                    "message": "Session token does not exists",
+                    "code": 404
+                }
+            }, 404, 'application/json');
+        }
+    });
+} else if (event.request.method == "POST" && event.resource == "place/add") {
+    var url = host +
+        "user/session?session_token=" +
+        event.request.headers["x-dreamfactory-session-token"][0];
+    platform.api.get(url, '', function (body, response) {
+        if (response.statusCode == 200) {
+            url = host +
+                "sidm/_table/USERS?filter=SYSTEM_ID%3D" +
+                JSON.parse(body).id +
+                "&" + apiKeyScript;
+            platform.api.get(url, '', function (body, response) {
+                if (response.statusCode == 200) {
+                    url = host +
+                        "sidm/_table/PLACES" +
+                        "?fields=*" +
+                        "&" + apiKeyScript;
+                    event.request.payload.resource[0].OWNER_ID = JSON.parse(body).resource[0].ID;
+                    platform.api.post(url, event.request.payload, '', function (body, response) {
+                        if (response.statusCode == 200) {
+                            event.setResponse(JSON.parse(body), response.statusCode, 'applicaton/json');
+                        } else {
+                            event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                        }
+                    });
+                } else {
+                    event.setResponse({
+                        "error": {
+                            "context": null,
+                            "message": "User does not exists",
+                            "code": 404
+                        }
+                    }, 404, 'application/json');
+                }
+            });
+        } else {
+            event.setResponse({
+                "error": {
+                    "context": null,
+                    "message": "Session token does not exists",
+                    "code": 404
+                }
+            }, 404, 'application/json');
+        }
+    });
+} else if (event.request.method == "POST" && event.resource == "namespace/add") {
+    var url = host +
+        "user/session?session_token=" +
+        event.request.headers["x-dreamfactory-session-token"][0];
+    platform.api.get(url, '', function (body, response) {
+        if (response.statusCode == 200) {
+            url = host +
+                "sidm/_table/USERS?filter=SYSTEM_ID%3D" +
+                JSON.parse(body).id +
+                "&" + apiKeyScript;
+            platform.api.get(url, '', function (body, response) {
+                if (response.statusCode == 200) {
+                    url = host +
+                        "sidm/_table/NAMESPACES" +
+                        "?fields=*" +
+                        "&" + apiKeyScript;
+                    event.request.payload.resource[0].OWNER_ID = JSON.parse(body).resource[0].ID;
+                    platform.api.post(url, event.request.payload, '', function (body, response) {
+                        if (response.statusCode == 200) {
+                            event.setResponse(JSON.parse(body), response.statusCode, 'applicaton/json');
+                        } else {
+                            event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                        }
+                    });
+                } else {
+                    event.setResponse({
+                        "error": {
+                            "context": null,
+                            "message": "User does not exists",
+                            "code": 404
+                        }
+                    }, 404, 'application/json');
+                }
+            });
+        } else {
+            event.setResponse({
+                "error": {
+                    "context": null,
+                    "message": "Session token does not exists",
+                    "code": 404
+                }
+            }, 404, 'application/json');
+        }
+    });
+} else if (event.request.method == "PATCH" && event.resource == "place/modify") {
+
+} else if (event.request.method == "PATCH" && event.resource == "namespace/modify") {
+
+} else if (event.request.method != "PATCH" && event.request.method != "POST" && event.request.method != "GET") {
+    throw new Error("Only HTTP GET, PATCH and POST are allowed on this service.");
+} else {
+    throw new Error("Resource does not exist");
+}
