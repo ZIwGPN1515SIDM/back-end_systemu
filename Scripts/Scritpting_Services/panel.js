6fd684d6b4416818b5f27b4fa19263c9c1fef085
Adam Hajduk <adam.hajduk@gmail.com>
2017-04-13 17:09:45 +0200

    Modify panel script
    Add code for changing places and namespaces

--- Scripts/Scritpting_Services/panel.js
+++ Scripts/Scritpting_Services/panel.js
@@ -178,9 +178,121 @@ if (event.request.method == "GET" && event.resource == "place") {
         }
     });
 } else if (event.request.method == "PATCH" && event.resource == "place/modify") {
-
+    if (event.request.payload.resource[0].hasOwnProperty('ID') && event.request.payload.resource.length == 1) {
+        var url = host +
+            "user/session?session_token=" +
+            event.request.headers["x-dreamfactory-session-token"][0];
+        platform.api.get(url, '', function (body, response) {
+            if (response.statusCode == 200) {
+                url = host +
+                    "sidm/_table/USERS?filter=SYSTEM_ID%3D" +
+                    JSON.parse(body).id +
+                    "&" + apiKeyScript;
+                platform.api.get(url, '', function (body, response) {
+                    if (response.statusCode == 200) {
+                        var userId = JSON.parse(body).resource[0].ID;
+                        url = host +
+                            "sidm/_table/PLACES" +
+                            "?filter=ID" +
+                            event.request.payload.resource[0].ID +
+                            "&" + apiKeyScript;
+                        platform.api.get(url, '', function (body, response) {
+                            if (response.statusCode == 200 && userId == JSON.parse(body).resource[0].OWNER_ID) {
+                                url = host +
+                                    "sidm/_table/PLACES" +
+                                    "?" + apiKeyScript;
+                                platform.api.patch(url, event.request.payload, '', function (body, response) {
+                                    if (response.statusCode == 200) {
+                                        event.setResponse(JSON.parse(body), response.statusCode, 'applicaton/json');
+                                    } else {
+                                        event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                                    }
+                                });
+                            } else if (response.statusCode == 200) {
+                                event.setResponse({"error": "user id mismatch"}, 400, 'applicaton/json');
+                            } else {
+                                event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                            }
+                        });
+                    } else {
+                        event.setResponse({
+                            "error": {
+                                "context": null,
+                                "message": "User does not exists",
+                                "code": 404
+                            }
+                        }, 404, 'application/json');
+                    }
+                });
+            } else {
+                event.setResponse({
+                    "error": {
+                        "context": null,
+                        "message": "Session token does not exists",
+                        "code": 404
+                    }
+                }, 404, 'application/json');
+            }
+        });
+    }
 } else if (event.request.method == "PATCH" && event.resource == "namespace/modify") {
-
+    if (event.request.payload.resource[0].hasOwnProperty('ID') && event.request.payload.resource.length == 1) {
+        var url = host +
+            "user/session?session_token=" +
+            event.request.headers["x-dreamfactory-session-token"][0];
+        platform.api.get(url, '', function (body, response) {
+            if (response.statusCode == 200) {
+                url = host +
+                    "sidm/_table/USERS?filter=SYSTEM_ID%3D" +
+                    JSON.parse(body).id +
+                    "&" + apiKeyScript;
+                platform.api.get(url, '', function (body, response) {
+                    if (response.statusCode == 200) {
+                        var userId = JSON.parse(body).resource[0].ID;
+                        url = host +
+                            "sidm/_table/NAMESPACES" +
+                            "?filter=ID" +
+                            event.request.payload.resource[0].ID +
+                            "&" + apiKeyScript;
+                        platform.api.get(url, '', function (body, response) {
+                            if (response.statusCode == 200 && userId == JSON.parse(body).resource[0].OWNER_ID) {
+                                url = host +
+                                    "sidm/_table/NAMESPACES" +
+                                    "?" + apiKeyScript;
+                                platform.api.patch(url, event.request.payload, '', function (body, response) {
+                                    if (response.statusCode == 200) {
+                                        event.setResponse(JSON.parse(body), response.statusCode, 'applicaton/json');
+                                    } else {
+                                        event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                                    }
+                                });
+                            } else if (response.statusCode == 200) {
+                                event.setResponse({"error": "user id mismatch"}, 400, 'applicaton/json');
+                            } else {
+                                event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+                            }
+                        });
+                    } else {
+                        event.setResponse({
+                            "error": {
+                                "context": null,
+                                "message": "User does not exists",
+                                "code": 404
+                            }
+                        }, 404, 'application/json');
+                    }
+                });
+            } else {
+                event.setResponse({
+                    "error": {
+                        "context": null,
+                        "message": "Session token does not exists",
+                        "code": 404
+                    }
+                }, 404, 'application/json');
+            }
+        });
+    }
 } else if (event.request.method != "PATCH" && event.request.method != "POST" && event.request.method != "GET") {
     throw new Error("Only HTTP GET, PATCH and POST are allowed on this service.");
 } else {
