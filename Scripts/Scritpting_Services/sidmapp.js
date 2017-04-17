091f9209d98400f85d72ad0aa11168bd0ded1ed8
Adam Hajduk <adam.hajduk@gmail.com>
2017-04-17 11:37:49 +0200

    Modify sidmapp script #1
    Add ability for changing NESLETTER field

--- Scripts/Scritpting_Services/sidmapp.js
+++ Scripts/Scritpting_Services/sidmapp.js
@@ -3,7 +3,7 @@ var apiKeyScript = "api_key=486bcd2b0b7cc55fbc3c16f1aadf041686d9cf68ce726b55c7c4
 if (event.request.method == "POST" && event.resource == "user") {
     var url = host
         + "sidm/_table/FB_USERS" +
-        "?fields=ID" +
+        "?fields=ID%2C%20NEWSLETTER" +
         "&filter=EMAIL%3D" +
         event.request.payload.resource[0].EMAIL +
         "&" + apiKeyScript;
@@ -16,7 +16,7 @@ if (event.request.method == "POST" && event.resource == "user") {
             } else if (response.statusCode == 200) {
                 url = host
                     + "sidm/_table/FB_USERS" +
-                    "?fields=ID" +
+                    "?fields=ID%2C%20NEWSLETTER" +
                     "&" + apiKeyScript;
                 platform.api.post(url, event.request.payload, '', function (body, response) {
                     if (response.statusCode == 200) {
@@ -33,4 +33,21 @@ if (event.request.method == "POST" && event.resource == "user") {
             }
         }
     );
+} else if (event.request.method == "PATCH" && event.resource == "user/modify"
+    && event.request.payload.resource.length == 1
+    && event.request.payload.resource[0].hasOwnProperty("ID")
+    && event.request.payload.resource[0].hasOwnProperty("NEWSLETTER")) {
+
+    var url = host
+        + "sidm/_table/FB_USERS" +
+        "?" + apiKeyScript;
+    platform.api.patch(url, event.request.payload, '', function (body, response) {
+        if (response.statusCode == 200) {
+            event.setResponse({"success": true}, response.statusCode, 'applicaton/json');
+        } else {
+            event.setResponse(JSON.parse(body), JSON.parse(body).error.code, 'applicaton/json');
+        }
+    });
+} else {
+    event.setResponse({"error": "Wrong query"}, 404, 'applicaton/json');
 }
